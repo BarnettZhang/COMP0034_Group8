1ef60681e03474da35975811e52e38a730737a01
ericz0920 <ericc0202@gmail.com>
2020-03-06 16:28:11 +0000

    Jinja2 and routes for srp and tsp

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,18 @@
-from flask import render_template, Blueprint, request, flash, redirect, url_for
-from sqlalchemy.exc import IntegrityError
-
-from app import db
-from app.main.forms import SignupForm
-from app.models import Course, Student
+from flask import render_template, Blueprint
 
 bp_main = Blueprint('main', __name__)
+
+
+@bp_main.route('/')
+def index():
+    return render_template('homepage.html')
+
+
+@bp_main.route('/take_survey_profile/', methods=['GET'])
+def take_survey_profile():
+    return render_template("tsp.html")
+
+
+@bp_main.route('/survey_review_profile/', methods=['GET'])
+def survey_review_profile():
+    return render_template("srp.html")
