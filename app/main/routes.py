ade3b72a310a9cbee822d3ef750ce620f5c37635
wsun <w.sunnn@gmail.com>
2020-03-12 17:12:39 +0000

    WS 2 ROUTES

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, Blueprint, request, flash, redirect, url_for, make_response, abort
+
 bp_main = Blueprint('main', __name__)
 
 
@@ -16,3 +17,13 @@ def create_survey():
 def finish_create_survey():
     flash('You have created a survey')
     return render_template('homepage.html')
+
+
+@bp_main.route('/privacy_policy', methods=['GET'])
+def privacy_policy():
+    return render_template("privacy_policy.html")
+
+
+@bp_main.route('/search_result', methods=['GET'])
+def search_result():
+    return render_template("search_result.html")
