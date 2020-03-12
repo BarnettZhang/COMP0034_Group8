cd8f48d9c52886d7e4982c593c4d908565b8af27
BarnettZhang <zcbezzh@ucl.ac.uk>
2020-03-12 18:27:40 +0000

    Full compiled functions

--- app/main/routes.py
+++ app/main/routes.py
@@ -12,9 +12,21 @@ def index():
 def edit_personal_info():
     return render_template("personal_info_edit.html")
 
+
+@bp_main.route('/create_survey')
+def create_survey():
+    return render_template("create_survey.html")
+
+
+@bp_main.route('/')
+def finish_create_survey():
+    return render_template('homepage.html')
+
+
 @bp_main.route('/search_result', methods=['GET'])
 def search_result():
     return render_template("search_result.html")
+
 
 @bp_main.route('/take_survey_profile/', methods=['GET'])
 def take_survey_profile():
@@ -24,3 +36,9 @@ def take_survey_profile():
 @bp_main.route('/survey_review_profile/', methods=['GET'])
 def survey_review_profile():
     return render_template("srp.html")
+
+
+@bp_main.route('/privacy_policy', methods=['GET'])
+def privacy_policy():
+    return render_template("privacy_policy.html")
+
