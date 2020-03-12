900718edf8ce02e03e866385c2aa9359d9babe77
vwcchan <zczlwc1@ucl.ac.uk>
2020-03-12 17:57:18 +0000

    update edit_personal_info and add routes

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-from flask import render_template, Blueprint, request, flash, redirect, url_for, make_response, abort
-=======
 from flask import render_template, Blueprint
->>>>>>> Ericcz
 
 bp_main = Blueprint('main', __name__)
 
@@ -12,25 +8,14 @@ def index():
     return render_template('homepage.html')
 
 
-@bp_main.route('/create_survey')
-def create_survey():
-    return render_template("create_survey.html")
-
-
-@bp_main.route('/')
-def finish_create_survey():
-    flash('You have created a survey')
-    return render_template('homepage.html')
-
-
-@bp_main.route('/privacy_policy', methods=['GET'])
-def privacy_policy():
-    return render_template("privacy_policy.html")
-
+@bp_main.route('/edit_personal_info/', methods=['GET'])
+def edit_personal_info():
+    return render_template("personal_info_edit.html")
 
 @bp_main.route('/search_result', methods=['GET'])
 def search_result():
     return render_template("search_result.html")
+
 @bp_main.route('/take_survey_profile/', methods=['GET'])
 def take_survey_profile():
     return render_template("tsp.html")
