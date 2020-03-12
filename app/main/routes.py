ba942ca15f98e136d32c0a71f6147df1facf4eb9
wsun <w.sunnn@gmail.com>
2020-03-12 17:02:44 +0000

    ws 1

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,3 +6,13 @@ from app.main.forms import SignupForm
 from app.models import Course, Student
 
 bp_main = Blueprint('main', __name__)
+
+
+@bp_main.route('/privacy_policy', methods=['GET'])
+def privacy_policy():
+    return render_template("privacy_policy.html")
+
+
+@bp_main.route('/search_result', methods=['GET'])
+def search_result():
+    return render_template("search_result.html")
