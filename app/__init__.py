2de0d7ca5f7a5958fe6df6ba3224c8f9f7edb53f
vwcchan <zczlwc1@ucl.ac.uk>
2020-03-12 16:56:03 +0000

    new version

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,12 @@ from config import DevConfig
 db = SQLAlchemy()
 
 
-def page_not_found(e):
-    return render_template('404.html'), 404
+# def page_not_found(e):
+#    return render_template('404.html'), 404
 
 
-def internal_server_error(e):
-    return render_template('500.html'), 500
+# def internal_server_error(e):
+#    return render_template('500.html'), 500
 
 
 def create_app(config_class=DevConfig):
--- app/main/routes.py
+++ app/main/routes.py
@@ -7,7 +7,8 @@ bp_main = Blueprint('main', __name__)
 def index():
     return render_template('homepage.html')
 
-@bp_main.route('/edit_personal_info/', method = ['GET'])
+
+@bp_main.route('/edit_personal_info/', methods=['GET'])
 def edit_personal_info():
     return render_template("personal_info_edit.html")
 
