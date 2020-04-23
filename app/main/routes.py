d9e5bda001d13bc228bec871165d268752fa79e3
ericz0920 <ericc0202@gmail.com>
2020-04-23 15:39:12 +0800

    new cookie used

--- app/main/routes.py
+++ app/main/routes.py
@@ -9,14 +9,14 @@ def index():
 
 
 @bp_main.route('/take_survey_profile/', methods=['GET'])
-def take_survey_profile():
-    if 'name' in request.cookies:
-        name = request.cookies.get('name')
+def take_survey_profile(name=""):
+    if 'username' in request.cookies:
+        name = request.cookies.get('username')
     return render_template("tsp.html", name=name)
 
 
 @bp_main.route('/survey_review_profile/', methods=['GET'])
-def survey_review_profile():
-    if 'name' in request.cookies:
-        name = request.cookies.get('name')
+def survey_review_profile(name=""):
+    if 'username' in request.cookies:
+        name = request.cookies.get('username')
     return render_template("srp.html", name=name)
