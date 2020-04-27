a12067ecf631653a43d9fe33029d34b373122fc1
wsun <w.sunnn@gmail.com>
2020-04-27 13:27:01 +0800

    4/27

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 from config import DevConfig
@@ -7,18 +7,31 @@ from config import DevConfig
 db = SQLAlchemy()
 
 
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 def create_app(config_class=DevConfig):
     """
     Creates an application instance to run
     :return: A Flask object
     """
     app = Flask(__name__)
-
-    # Configure the Flask app wth the configuration settings from a class in config.py
     app.config.from_object(config_class)
 
+    # Configure app wth the settings from config.py
+    app.config.from_object(config_class)
 
-    # Register Blueprints
+    # Initialise the database and create tables
+    db.init_app(app)
+    from app.models import Survey, Answer, Question, User
+    with app.app_context():
+        db.create_all()
+
     from app.main.routes import bp_main
     app.register_blueprint(bp_main)
 
--- config.py
+++ config.py
@@ -1,26 +1,41 @@
 """Flask config class."""
+
 from os.path import dirname, abspath, join
+
+CWD = dirname(abspath(__file__))
 
 
 class Config(object):
-    """Set Flask base configuration"""
-    SECRET_KEY = 'dfdQbTOExternjy5xmCNaA'
-
-    # General Config
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + join(CWD, 'qnr.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'dfdQbTOExternjy5xmCNaA'
 
-    # Forms config
-    WTF_CSRF_SECRET_KEY = 'dfdQbTOExternjy5xmCNaA'
 
 class ProdConfig(Config):
+    # The following are fictitious details for a MySQL server database! Included to illustrate the syntax.
+    DB_SERVER = '192.168.19.32'
+    SQLALCHEMY_DATABASE_URI = 'mysql://user@{}/foo'.format(DB_SERVER)
     DEBUG = False
     TESTING = False
 
 
 class TestConfig(Config):
+    DEBUG = True
     TESTING = True
+    #  In memory database
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    #  To allow forms to be submitted from the tests without the CSRF token
+    WTF_CSRF_ENABLED = False
 
 
 class DevConfig(Config):
     DEBUG = True
+
+
+app_config = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'testing': TestConfig
+}
