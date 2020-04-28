5d8b7a7769a0a04cea95fcb4ab78062541893d5f
ericz0920 <ericc0202@gmail.com>
2020-04-28 15:51:56 +0800

    base.html changed into login/logout version Signup and Login form/routes/html are no bug but cannot connect with db
    Signup and Login functionality issue, db connection issue

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from config import DevConfig
+from flask_session import Session
 
 # The SQLAlchemy object is defined globally
 db = SQLAlchemy()
+login_manager = LoginManager()
+sess = Session()
 
 
 def page_not_found(e):
@@ -21,10 +24,13 @@ def create_app(config_class=DevConfig):
     :return: A Flask object
     """
     app = Flask(__name__)
-    app.config.from_object(config_class)
 
     # Configure app wth the settings from config.py
     app.config.from_object(config_class)
+
+    # Initialise plugins
+    db.init_app(app)
+    login_manager.init_app(app)
 
     # Initialise the database and create tables
     db.init_app(app)
@@ -32,7 +38,15 @@ def create_app(config_class=DevConfig):
     with app.app_context():
         db.create_all()
 
+    # Register error handlers
+    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(500, internal_server_error)
+
+    # Register Blueprints
     from app.main.routes import bp_main
     app.register_blueprint(bp_main)
+
+    from app.auth.routes import bp_auth
+    app.register_blueprint(bp_auth)
 
     return app
--- app/main/forms.py
+++ app/main/forms.py
@@ -8,21 +8,6 @@ from app import db
 from app.models import Survey, User, Question, Answer
 
 
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
-
-
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
-
-
 class CreateSurvey(FlaskForm):
     target_gender = SelectField('Gender',
                                 choices=[('male', 'Male'), ('female', 'Female'), ('all gender', 'All Gender')])
