2c044db4a5721c4fb6ed95f849a0f9f849da0742
BarnettZhang <zcbezzh@ucl.ac.uk>
2020-05-01 18:52:48 +0800

    Reformatted

--- app/__init__.py
+++ app/__init__.py
@@ -36,7 +36,7 @@ def create_app(config_class=DevConfig):
     db.init_app(app)
     from app.models import Survey, Answer, User
     with app.app_context():
-        #db.drop_all()
+        # db.drop_all()
         db.create_all()
 
     # Register error handlers
