f42f9448afd40fbc62efd64e5aa9a124f9ea6365
ericz0920 <ericc0202@gmail.com>
2020-05-01 17:06:12 +0800

    remove cookie setting and take survey profile

--- app/main/routes.py
+++ app/main/routes.py
@@ -200,13 +200,6 @@ def answer_survey():
     return render_template("answer_survey.html", answer=answer)
 
 
-@bp_main.route('/take_survey_profile/', methods=['GET'])
-def take_survey_profile(name=""):
-    if 'username' in request.cookies:
-        name = request.cookies.get('username')
-    return render_template("tsp.html", name=name)
-
-
 @bp_main.route('/survey_review_profile/', methods=['GET'])
 def survey_review_profile():
     if 'username' in request.cookies:
